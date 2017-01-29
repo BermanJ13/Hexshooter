b24434e04f7b75f2b2a4bd3b5656ece99e5098b5
Stefen Lewis <sml5527@g.rit.edu>
2017-01-28 21:36:43 -0500

    Movement and FIeld Complete

---
+++ Hexshooters/Assets/Scripts/FieldManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldManager : MonoBehaviour 
+{
+	public Transform playerPanel;
+	public Transform enemyPanel;
+	public Transform player;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//Creates the Grid
+		for (int y = 0; y < 5; y++) 
+		{
+			for (int x = 0; x < 10; x++) 
+			{
+				//Checks whether the current panel is for the enmy or player side
+				if(x<5)
+				{
+					Instantiate(playerPanel, new Vector3(x, y, 0), Quaternion.identity);
+					//sPawns the Player
+					if(y==2 && x==0)
+						Instantiate(player, new Vector3(x, y, 0), Quaternion.identity);
+				}
+				else
+					Instantiate(enemyPanel, new Vector3(x, y, 0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Hexshooters/Assets/Scripts/Player.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public LayerMask mask = LayerMask.NameToLayer("PlayerPanel");
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	void FixedUpdate() 
+	{
+		//Moves the character
+		movement();
+
+
+	}
+	void movement()
+	{
+		bool moveRight = false;
+		bool moveLeft  = false;
+		bool moveUp  = false;
+		bool moveDown = false;
+		//Checks for Left and RIght Movement
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			Collider2D[] hitColliders = Physics2D.OverlapCircleAll(new Vector3(transform.position.x + 1, transform.position.y),0.2f);
+			//Checks whether or not something is in the way or if the desired spot is within the player.
+			foreach( Collider2D c in hitColliders)
+			{
+				if (c.gameObject.tag == "playerZone")
+				{
+					moveRight = true;
+				}
+				if(c.gameObject.tag == "obstacle")
+				{
+					moveRight = false;
+				}
+				
+				if(c.gameObject.tag == "enemyZone")
+				{
+					moveRight = false;
+				}
+				
+				if(c.gameObject.tag == "enemy")
+				{
+					moveRight = false;
+				}
+			}
+			//Performs the movement if possible
+			if(moveRight)
+				transform.position = new Vector2 (transform.position.x + 1, transform.position.y);
+		} 
+		else if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			Collider2D[] hitColliders = Physics2D.OverlapCircleAll(new Vector3(transform.position.x - 1, transform.position.y),0.2f);
+			foreach( Collider2D c in hitColliders)
+			{
+				//Checks whether or not something is in the way or if the desired spot is within the player.
+				if (c.gameObject.tag == "playerZone")
+				{
+					moveLeft = true;
+				}
+				if(c.gameObject.tag == "obstacle")
+				{
+					moveLeft = false;
+				}
+				
+				if(c.gameObject.tag == "enemyZone")
+				{
+					moveLeft = false;
+				}
+				
+				if(c.gameObject.tag == "enemy")
+				{
+					moveLeft = false;
+				}
+			}
+			//Performs the movement if possible
+			if(moveLeft)
+				transform.position = new Vector2 (transform.position.x - 1, transform.position.y);
+		}
+		//Checks for Up and Down Movement
+		if (Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
+			Collider2D[] hitColliders = Physics2D.OverlapCircleAll(new Vector3(transform.position.x, transform.position.y + 1),0.2f);
+			foreach( Collider2D c in hitColliders)
+			{
+				//Checks whether or not something is in the way or if the desired spot is within the player.
+				if (c.gameObject.tag == "playerZone")
+				{
+					moveUp = true;
+				}
+				if(c.gameObject.tag == "obstacle")
+				{
+					moveUp = false;
+				}
+				
+				if(c.gameObject.tag == "enemyZone")
+				{
+					moveUp = false;
+				}
+				
+				if(c.gameObject.tag == "enemy")
+				{
+					moveUp = false;
+				}
+			}
+			//Performs the movement if possible
+			if(moveUp)
+				transform.position = new Vector2 (transform.position.x, transform.position.y + 1);
+		} 
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		{
+			Collider2D[] hitColliders = Physics2D.OverlapCircleAll(new Vector3(transform.position.x, transform.position.y - 1),0.2f);
+			foreach( Collider2D c in hitColliders)
+			{
+				//Checks whether or not something is in the way or if the desired spot is within the player.
+				if (c.gameObject.tag == "playerZone")
+				{
+					moveDown = true;
+				}
+				if(c.gameObject.tag == "obstacle")
+				{
+					moveDown = false;
+				}
+				
+				if(c.gameObject.tag == "enemyZone")
+				{
+					moveDown = false;
+				}
+				
+				if(c.gameObject.tag == "enemy")
+				{
+					moveDown = false;
+				}
+			}
+			//Performs the movement if possible
+			if(moveDown)
+				transform.position = new Vector2 (transform.position.x, transform.position.y - 1);
+		}
+	}
+}
