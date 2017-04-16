d3e7d358848cd525cc94ad91912974684c792fd3
Greg-Carrobis <gtc3509@g.rit.edu>
2017-04-16 10:58:12 -0400

    Preload Scene preloading us to the Menu

---
+++ Hexshooters/Assets/Scripts/Preload/Load.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load : MonoBehaviour {
+
+    // Use this for initialization
+    void Awake()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+}
