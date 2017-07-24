ee7539e3fe550fc691a73072ce9c98731a52ca69
Greg-Carrobis <gtc3509@g.rit.edu>
2017-07-24 16:07:14 -0400

    Imperfect Chasing for Wandering Monster. Sort of already hate it so I'll probably redo it

--- Hexshooters/Assets/Enemies/Scripts/OverEnemy.cs
+++ Hexshooters/Assets/Enemies/Scripts/OverEnemy.cs
@@ -15,6 +15,8 @@ public class OverEnemy : MonoBehaviour {
 
     public Vector2 startPosition;
 
+    public bool returnPos;
+
     [Header("Behavior")]
     public WanderType movement;
     public int patrolDistance;
@@ -32,70 +34,85 @@ public class OverEnemy : MonoBehaviour {
             {
                 patrolDistance = 5;
             }
-            if (speed<=0)
-            {
-                speed = 1;
-            }
         }
-        if (chase == true)
+        if (chaseDistance<=0)
         {
-            if (chaseDistance<=0)
-            {
-                chaseDistance = 5;
-            }
-            if (speed <= 0)
-            {
-                speed = 1;
-            }
+            chaseDistance = 1;
+        }
+        if (speed <= 0)
+        {
+            speed = 1;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        switch (movement)
+
+        if (!chase)
         {
-            case WanderType.patrolHorizontal:
-                
-                if (!reverse)
-                    this.transform.position = new Vector2(this.transform.position.x + 1, this.transform.position.y);
-                else
-                    this.transform.position = new Vector2(this.transform.position.x - 1, this.transform.position.y);
-
-                if (this.transform.position.x >= (startPosition.x + (patrolDistance/2)))
-                {
-                    reverse = true;
-                }
-                else if (this.transform.position.x <=(startPosition.x - (patrolDistance/2)))
-                {
-                    reverse = false;
-                }
-                
-                break;
-            case WanderType.patrolVertical:
-                if (!reverse)
-                    this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 1);
-                else
-                    this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 1);
-
-                if (this.transform.position.y >= (startPosition.y + (patrolDistance / 2)))
-                {
-                    reverse = true;
-                }
-                else if (this.transform.position.y <= (startPosition.y - (patrolDistance / 2)))
-                {
-                    reverse = false;
-                }
-                break;
-            case WanderType.stand:
-            default:
-                break;
+            if (Vector2.Distance(startPosition, target.transform.position) <= chaseDistance)
+                chase = true;
         }
+        else
+        {
+            if (Vector2.Distance(startPosition, target.transform.position) > chaseDistance)
+                chase = false;
+        }
+
         if (chase)
         {
-            stopChase(chaseDistance);
+                if (this.transform.position.x < target.transform.position.x)
+                    this.transform.position = new Vector2(this.transform.position.x+1, this.transform.position.y);
+                if (this.transform.position.x > target.transform.position.x)
+                    this.transform.position = new Vector2(this.transform.position.x - 1, this.transform.position.y);
+                if (this.transform.position.y < target.transform.position.y)
+                    this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 1);
+                if (this.transform.position.y > target.transform.position.y)
+                    this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 1);
+        }
+        else
+        {
+            switch (movement)
+            {
+                case WanderType.patrolHorizontal:
+
+                    if (!reverse)
+                        this.transform.position = new Vector2(this.transform.position.x + 1, this.transform.position.y);
+                    else
+                        this.transform.position = new Vector2(this.transform.position.x - 1, this.transform.position.y);
+
+                    if (this.transform.position.x >= (startPosition.x + (patrolDistance / 2)))
+                    {
+                        reverse = true;
+                    }
+                    else if (this.transform.position.x <= (startPosition.x - (patrolDistance / 2)))
+                    {
+                        reverse = false;
+                    }
+
+                    break;
+                case WanderType.patrolVertical:
+                    if (!reverse)
+                        this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 1);
+                    else
+                        this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 1);
+
+                    if (this.transform.position.y >= (startPosition.y + (patrolDistance / 2)))
+                    {
+                        reverse = true;
+                    }
+                    else if (this.transform.position.y <= (startPosition.y - (patrolDistance / 2)))
+                    {
+                        reverse = false;
+                    }
+                    break;
+                case WanderType.stand:
+                default:
+                    break;
+            }
         }
 	}
-    public void stopChase(int chaseDistance)
+    public void stopChase()
     {
 
     }
