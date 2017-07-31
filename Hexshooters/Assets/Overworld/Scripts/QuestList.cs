b8e206a89e61dd8cab7afdc93fa1cd61e677525c
JAMESBERMAN\Meep <bermanj1313@gmail.com>
2017-07-31 15:12:10 -0400

    Quest class made with everything but the reward function atm. master QuestList class made as well that has multiple lists of quests that are decided by location with the only function atm being adding quests. atm working on that with the interactivity of the quest menu.

---
+++ Hexshooters/Assets/Overworld/Scripts/QuestList.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestList : MonoBehaviour {
+
+	//location names atm are Idledale, Ragged Bend, Coal Creek atm
+	private List<Quest>idledale = new List<Quest>();
+	private List<Quest>raggedBend = new List<Quest>();
+	private List<Quest>coalCreek = new List<Quest>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//public access to each of the quests so that the user interface can see them
+	public List<Quest> Idledale
+	{
+		get{ return this.idledale;}
+	}
+
+	public List<Quest> RaggedBend
+	{
+		get{ return this.raggedBend;}
+	}
+
+	public List<Quest> CoalCreek
+	{
+		get{ return this.coalCreek;}
+	}
+
+	//Parameters: quest
+	//Purpose: adds a quest to the quest list
+	//known errors: unknown atm
+	public void AddQuest(Quest newQuest)
+	{
+		//adds the quest into the correct town list
+		Quest add = newQuest;
+		if (add.Location == "Idledale") 
+		{
+			Idledale.Add (add);
+		}
+		if (add.Location == "Ragged Bend")
+		{
+			raggedBend.Add (add);
+		}
+		if(add.Location == "Coal Creek")
+		{
+			coalCreek.Add (add);
+		}
+	}
+}
