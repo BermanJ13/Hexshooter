a059aea1cdfb38ee02af5cf732272ee29691e25a
Greg-Carrobis <gtc3509@g.rit.edu>
2017-07-31 14:42:58 -0400

    Better Wandering Monster

--- Hexshooters/Assets/Enemies/Scripts/OverEnemy.cs
+++ Hexshooters/Assets/Enemies/Scripts/OverEnemy.cs
@@ -3,117 +3,187 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum WanderType
+public enum WanderType //new types can be added
 {
     stand,
-    patrolHorizontal,
-    patrolVertical,
+    patrolLine,
+    patrolZag,
+    patrolDiamond, 
+    meander, //make random targets
+    lurk //stand and then chase
 }
+
+
 public class OverEnemy : MonoBehaviour {
 
-    public OverPlayer target;
+    public OverPlayer player;
 
     public Vector2 startPosition;
 
-    public bool returnPos;
+    public GameObject target;
+
+    public int pointTraveled;
+    public bool switchFlag;
 
     [Header("Behavior")]
     public WanderType movement;
-    public int patrolDistance;
-    public bool chase;
-    public int chaseDistance;
-    public int speed;
-    public bool reverse;
+    public float horizontalDistance;
+    public float verticalDistance;
+    public float speed; //between .005 and .3
     
     // Use this for initialization
 	void Start () {
+        //target = this.GetComponentInChildren<GameObject>();
+        player = FindObjectOfType<OverPlayer>();
         startPosition = this.transform.position;
-		if (!movement.Equals(WanderType.stand))
-        {
-            if (patrolDistance<=0)
-            {
-                patrolDistance = 5;
-            }
-        }
-        if (chaseDistance<=0)
-        {
-            chaseDistance = 1;
-        }
+        pointTraveled = 0;
+        switchFlag = false;
+
         if (speed <= 0)
         {
-            speed = 1;
+            speed = .005f;
         }
+        else if (speed > .3)
+        {
+            speed = .3f;
+        }
+
+        switchTarget(movement);
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        if (!chase)
-        {
-            if (Vector2.Distance(startPosition, target.transform.position) <= chaseDistance)
-                chase = true;
-        }
-        else
-        {
-            if (Vector2.Distance(startPosition, target.transform.position) > chaseDistance)
-                chase = false;
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+    void move()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y), 0.1f);
 
-        if (chase)
+        if (this.transform.position.x <= target.transform.position.x)
         {
-                if (this.transform.position.x < target.transform.position.x)
-                    this.transform.position = new Vector2(this.transform.position.x+1, this.transform.position.y);
-                if (this.transform.position.x > target.transform.position.x)
-                    this.transform.position = new Vector2(this.transform.position.x - 1, this.transform.position.y);
-                if (this.transform.position.y < target.transform.position.y)
-                    this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 1);
-                if (this.transform.position.y > target.transform.position.y)
-                    this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 1);
-        }
-        else
-        {
-            switch (movement)
+            this.transform.position = new Vector2(this.transform.position.x + speed, this.transform.position.y);
+            foreach (Collider2D c in hitColliders)
             {
-                case WanderType.patrolHorizontal:
-
-                    if (!reverse)
-                        this.transform.position = new Vector2(this.transform.position.x + 1, this.transform.position.y);
-                    else
-                        this.transform.position = new Vector2(this.transform.position.x - 1, this.transform.position.y);
-
-                    if (this.transform.position.x >= (startPosition.x + (patrolDistance / 2)))
-                    {
-                        reverse = true;
-                    }
-                    else if (this.transform.position.x <= (startPosition.x - (patrolDistance / 2)))
-                    {
-                        reverse = false;
-                    }
-
-                    break;
-                case WanderType.patrolVertical:
-                    if (!reverse)
-                        this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 1);
-                    else
-                        this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 1);
-
-                    if (this.transform.position.y >= (startPosition.y + (patrolDistance / 2)))
-                    {
-                        reverse = true;
-                    }
-                    else if (this.transform.position.y <= (startPosition.y - (patrolDistance / 2)))
-                    {
-                        reverse = false;
-                    }
-                    break;
-                case WanderType.stand:
-                default:
-                    break;
+                if (c.gameObject.tag == "Boundary")
+                {
+                    this.transform.position = new Vector2(this.transform.position.x - speed,this.transform.position.y);
+                }
+                else if (c.gameObject.tag == "EnemyTarget")
+                {
+                    switchFlag = true;
+                }
             }
         }
-	}
-    public void stopChase()
-    {
+        else
+        {
+            this.transform.position = new Vector2(this.transform.position.x - speed, this.transform.position.y);
+            foreach (Collider2D c in hitColliders)
+            {
+                if (c.gameObject.tag == "Boundary")
+                {
+                    this.transform.position = new Vector2(this.transform.position.x + speed, this.transform.position.y);
+                }
+                else if (c.gameObject.tag == "EnemyTarget")
+                {
+                    switchFlag = true;
+                }
+            }
+        }
 
+        if (this.transform.position.y <= target.transform.position.y)
+        {
+            this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + speed);
+            foreach (Collider2D c in hitColliders)
+            {
+                if (c.gameObject.tag == "Boundary")
+                {
+                    this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - speed);
+                }
+                else if (c.gameObject.tag == "EnemyTarget")
+                {
+                    switchFlag = true;
+                }
+            }
+        }
+        else
+        {
+            this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - speed);
+            foreach (Collider2D c in hitColliders)
+            {
+                if (c.gameObject.tag == "Boundary")
+                {
+                    this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + speed);
+                }
+                else if (c.gameObject.tag == "EnemyTarget")
+                {
+                    switchFlag = true;
+                }
+            }
+        }
+        if (switchFlag)
+        {
+            switchFlag = false;
+            switchTarget(movement);
+        }
     }
+    void switchTarget(WanderType wander)
+    {
+        Vector2 pos = startPosition;
+        switch (wander)
+        {
+            case WanderType.patrolRectangle:
+                if (pointTraveled == 0)
+                    target.transform.position = new Vector2(pos.x + horizontalDistance, pos.y);
+                else if (pointTraveled == 1)
+                    target.transform.position = new Vector2(pos.x, pos.y + verticalDistance);
+                else if (pointTraveled == 2)
+                    target.transform.position = new Vector2(pos.x - horizontalDistance, pos.y);
+                else if (pointTraveled == 3)
+                    target.transform.position = new Vector2(pos.x, pos.y - verticalDistance);
+
+                if (pointTraveled < 3)
+                    pointTraveled++;
+                else
+                    pointTraveled = 0;
+                break;
+            case WanderType.patrolElbow:
+                if (pointTraveled == 0)
+                    target.transform.position = new Vector2(pos.x + horizontalDistance, pos.y);
+                else if (pointTraveled == 1)
+                    target.transform.position = new Vector2(pos.x, pos.y + verticalDistance);
+                else if (pointTraveled == 2)
+                    target.transform.position = new Vector2(pos.x, pos.y - verticalDistance);
+                else if (pointTraveled == 3)
+                    target.transform.position = new Vector2(pos.x - horizontalDistance, pos.y);
+
+                if (pointTraveled < 3)
+                    pointTraveled++;
+                else
+                    pointTraveled = 0;
+                break;
+            case WanderType.patrolLine:
+                if (pointTraveled == 0)
+                    target.transform.position = new Vector2(pos.x + horizontalDistance, pos.y + verticalDistance);
+                else if (pointTraveled == 1)
+                    target.transform.position = new Vector2(pos.x - horizontalDistance, pos.y - verticalDistance);
+
+                if (pointTraveled == 0)
+                    pointTraveled++;
+                else
+                    pointTraveled = 0;
+                break;
+            case WanderType.meander:
+                target.transform.position = new Vector2(startPosition.x+Random.Range(-horizontalDistance, horizontalDistance), startPosition.y+ Random.Range(-verticalDistance, verticalDistance));
+                break;
+            case WanderType.lurk:
+                if (Vector2.Distance(player.transform.position, pos) < Vector2.Distance(new Vector2(horizontalDistance / 2, verticalDistance / 2), pos))
+                    target.transform.position = new Vector2(player.transform.position.x, player.transform.position.y);
+                break;
+            case WanderType.stand:
+            default:
+                break;
+        }
+    }
+        
 }
