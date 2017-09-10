ea2e97d355ec78c0762eb7edf65d89aaed11ca17
HJAgresta <hjagresta@gmail.com>
2017-09-10 13:13:38 -0400

    bandits are alot smarter, started trains spell, sacman has some basic ai

--- Hexshooters/Assets/Scripts/Spells/Trains.cs
+++ Hexshooters/Assets/Scripts/Spells/Trains.cs
@@ -1,16 +1,81 @@
-﻿using System.Collections;
+﻿using System;
+using System.Threading;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trains : MonoBehaviour {
+public class Trains : Spell
+{
+    private GameObject[] enemyPanels;
+    private GameObject[] playerPanels;
+    private bool targetNeeded;
+    bool inBounds;
+    public Transform track;
+    Vector2 target;
+    Vector2 position;
+    Collider2D[] colliders;
+    private bool created;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    int powerLvl = 1;
+
+    // Use this for initialization
+    new void Start()
+    {
+        base.Start();
+        enemyPanels = GameObject.FindGameObjectsWithTag("enemyZone");
+        playerPanels = GameObject.FindGameObjectsWithTag("playerZone");
+        targetNeeded = true;
+        inBounds = false;
+        created = false;
+
+
+        Instantiate(track, gameObject.transform.position, gameObject.transform.rotation);
+
+        trackBehind(transform.position);
+        trackAhead(transform.position);
+    }
+
+    void trackBehind(Vector3 curPos)
+    {
+        Collider2D[] colliders = Physics2D.OverlapAreaAll(new Vector2(curPos.x - 1, curPos.y), new Vector2(curPos.x - 1, curPos.y));
+        foreach (Collider2D c in colliders)
+        {
+            if (c.gameObject.tag == "Track")
+            {
+                powerLvl++;
+                trackBehind(curPos);
+            }
+        }
+    }
+    void trackAhead(Vector3 curPos)
+    {
+        Collider2D[] colliders = Physics2D.OverlapAreaAll(new Vector2(curPos.x + 1, curPos.y), new Vector2(curPos.x + 1, curPos.y));
+        foreach (Collider2D c in colliders)
+        {
+            if (c.gameObject.tag == "Track")
+            {
+                powerLvl++;
+                trackAhead(curPos);
+            }
+        }
+    }
+    // Update is called once per frame
+    new void spellUpdate()
+    {
+        base.spellUpdate();
+    }
+
+    public override void movement(int weapon)
+    {
+    }
+
+    public override void hitBehavior(int weapon)
+    {
+       
+    }
+
+    public override void setDescription(int weapon)
+    {
+
+    }
 }
